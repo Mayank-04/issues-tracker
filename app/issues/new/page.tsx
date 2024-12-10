2fb4d34f149c367530d72d1b4ac099a78621d0a4
Mayank Gautam <macbookpro@Macbooks-MacBook-Pro.local>
2024-12-10 16:48:41 +0530

    Implement Client-Side Validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,23 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+// This type is used to define the shape of the form data withput defining a schema manually and changing it every time when there is a change in the schema.
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState : { errors }} = useForm<IssueForm>({
+    resolver : zodResolver(createIssueSchema)
+  });
   const [error, setError] = useState('');
 
   return (
@@ -38,6 +41,7 @@ const NewIssuePage = () => {
       )}
     >
       <TextField.Root placeholder="Title" {...register("title")} />
+      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
       <Controller
         name="description"
         control={control}
@@ -45,6 +49,7 @@ const NewIssuePage = () => {
           <SimpleMDE placeholder="Description" {...field} />
         )}
       />
+      {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
       <Button>Submit New Issue</Button>
     </form>
     </div>
