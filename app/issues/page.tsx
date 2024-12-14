2bcbbe556bdd804911565790997fd5df181dd762
Mayank Gautam <macbookpro@Macbooks-MacBook-Pro.local>
2024-12-14 21:11:40 +0530

    Display the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@radix-ui/themes"
-import Link from "next/link"
-const IssuesPage = () => {
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import prisma from "@/prisma/client";
+
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-        <Button><Link href='/issues/new'>New Issue</Link></Button>
-    </div>
-  )
-}
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+      <Table.Root variant="ghost">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-export default IssuesPage; 
+        <Table.Body>
+          
+            {issues.map(issue => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>
+                  {issue.title}
+                </Table.Cell>
+                <Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
+                <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default IssuesPage;
